7d1d62ccb756a865c0c0149297db2542665cb1f8
Timothy Moore <mtimothy983@gmail.com>
2017-12-06 10:07:36 -0800

    Add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root" 
+exit 1
+fi
+
+rm -rf ~/website_tmp
+git clone --depth=1 --branch=master https://github.com/Tjstretchalot/LoansBot-Site-New ~/website_tmp
+rsync -avh ~/website_tmp/html /var/www/ --delete
+rsync -avh ~/website_tmp/includes /var/www/ --delete
+rm -rf ~/website_tmp
