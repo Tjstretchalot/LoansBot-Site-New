d10b7c8c53182f569333cad12bf2545e45d2fba0
Timothy Moore <mtimothy983@gmail.com>
2017-12-06 11:55:29 -0800

    Actually commit navigation

---
+++ includes/navigation.php
@@ -0,0 +1,48 @@
+<?php
+  $links = array();
+
+  $links[] = array(
+    'link' => '/index.php',
+    'name' => 'Index'
+  );
+
+  if($logged_in_user === null) {
+    $links[] = array(
+      'link' => '/login.php',
+      'name' => 'Login'
+    );
+  }else {
+    $links[] = array(
+      'link' => '/logout.php',
+      'name' => 'Logout'
+    );
+  }
+?>
+<!-- navigation start -->
+
+<div class="container-fluid">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">USL</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle Navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarContent">
+      <ul class="navbar-nav mr-auto">
+        <?php foreach($pages as $key=>$page): ?>
+        <?php if ($_SERVER['REQUEST_URI'] == $page['link'] || ($_SERVER['REQUEST_URI'] == '/' && $page['link'] == '/index.php')): ?>
+          <li class="nav-item active"> 
+            <a class="nav-link" href="#"><?= $page['name'] ?> <span class="sr-only">(current)</span></a>
+          </li>
+        <?php else: ?>
+          <li class="nav-item">
+            <a class="nav-link" href="<?= $page['link'] ?>"><?= $page['name'] ?></a>
+          </li>
+        <?php endif; ?>
+        <?php endforeach; ?>
+      </ul>
+    </div>
+  </nav>
+</div>
+
+<!-- navigation end -->
