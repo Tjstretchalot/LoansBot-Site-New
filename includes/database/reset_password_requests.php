19d294c1a1e807f43d6de638b3ce96ded020b0b5
Timothy Moore <mtimothy983@gmail.com>
2017-12-07 10:47:13 -0800

    Fix reset password request mapping

--- includes/database/reset_password_requests.php
+++ includes/database/reset_password_requests.php
@@ -32,7 +32,7 @@
 
     public static function insert($sql_conn, $user_id, $reset_code) {
       $err_prefix = 'ResetPasswordRequestMapping::insert';
-      check_db_error($sql_conn, $err_prefix, $stmt = $sql_conn->prepare('INSERT INTO reset_password_requests (user_id, reset_code, reset_code_sent, reset_code_used, created_at, updated_at) VALUES (?, ?, NULL, NULL, NOW(), NOW())'));
+      check_db_error($sql_conn, $err_prefix, $stmt = $sql_conn->prepare('INSERT INTO reset_password_requests (user_id, reset_code, reset_code_sent, reset_code_used, created_at, updated_at) VALUES (?, ?, 0, 0, NOW(), NOW())'));
       check_db_error($sql_conn, $err_prefix, $stmt->bind_param('is', $user_id, $reset_code));
       check_db_error($sql_conn, $err_prefix, $stmt->execute());
 
@@ -46,7 +46,7 @@
     public static function update_used($sql_conn, $id) {
       $err_prefix = 'ResetPasswordRequestMapping::update_used';
 
-      check_db_error($sql_conn, $err_prefix, $stmt = $sql_conn->prepare('UPDATE reset_password_requests SET reset_code_used=NOW(), updated_at=NOW() WHERE id=?'));
+      check_db_error($sql_conn, $err_prefix, $stmt = $sql_conn->prepare('UPDATE reset_password_requests SET reset_code_used=1, updated_at=NOW() WHERE id=?'));
       check_db_error($sql_conn, $err_prefix, $stmt->bind_param('i', $id));
       check_db_error($sql_conn, $err_prefix, $stmt->execute());
 
