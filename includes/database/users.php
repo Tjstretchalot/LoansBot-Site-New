6b1f67941d09a8802e84529a19e535ac89a4a95a
Timothy Moore <mtimothy983@gmail.com>
2017-12-08 09:32:45 -0800

    database users.php fixes

--- includes/database/users.php
+++ includes/database/users.php
@@ -44,7 +44,7 @@
       return new ArrayObject($row, ArrayObject::ARRAY_AS_PROPS); 
     }
 
-    public static function create_by_username($isql_conn, $username) {
+    public static function create_by_username($sql_conn, $username) {
       $err_prefix = 'UserMapping::create_by_username';
 
       check_db_error($sql_conn, $err_prefix, $stmt = $sql_conn->prepare('INSERT INTO users (auth, claimed, claim_link_sent_at, created_at, updated_at) values (0, null, now(), now())'));
