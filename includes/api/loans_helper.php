e10f7d9182d79c6dc4218039691c51f2eb3d4b68
Timothy Moore <mtimothy983@gmail.com>
2017-12-08 16:24:33 -0800

    use mod permission

--- includes/api/loans_helper.php
+++ includes/api/loans_helper.php
@@ -573,7 +573,7 @@ class ParameterParser {
       $helper->add_callback($result);
     }else {
       $result = new LoanQueryCallback('include_deleted_at', array(), null, null, null, null, null, null, null);
-      $result->authorization_callback = function($helper, $auth) {
+      $result->authorization_callback = function($helper, $auth) use($MODERATOR_PERMISSION) {
         if($auth < $MODERATOR_PERMISSION) {
           return array('error_ident' => 'NOT_AUTHORIZED', 'error_message' => 'You do not have permission to view deleted loans');
         }
