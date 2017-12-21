dbdbbb2a3b408873c98ec679d345ac7972dabd7b
Timothy Moore <mtimothy984@gmail.com>
2017-12-21 21:55:14 +0000

    Add before time to query2

--- html/js/query2_query_parameters.js
+++ html/js/query2_query_parameters.js
@@ -161,4 +161,24 @@ var query2_parameters = {};
       return container;
     }
   });
+
+  query2_parameters.before_time = apply_defaults({
+    param_name: "before_time",
+    name: "Created Before",
+    construct_html: function(time) {
+      if(!time)
+        time = new Date();
+      else
+        time = new Date(time);
+      var container = generate_container(this.param_name);
+      var label = generate_label(this.param_name, "Created Before");
+      var help_block = generate_simple_help_block(this.param_name, "Restrict the results to loans that were created strictly before midnight on the specified date. The created time, for loans after 2015, is the timestamp for the comment that generated the loan. For loans prior to 2015, created_at is the time when the loan was added to the database. For loans prior to 2015, this is using PST, for other loans it matches the reddit timezone.");
+      var control = generate_input_control(this.param_name, "date", "Created before date");
+      control.attr('value', time.toISOString().slice(0, 10));
+      var remove_button = generate_remove_button(this.param_name);
+
+      combine_elements(this.param_name, container, { label: label, control: control, help_block: help_block, remove_button: remove_button });
+      return container;
+    }
+  });
 })();
