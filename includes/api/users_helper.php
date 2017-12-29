aced6d5b7983ce443ced70e1ea4c73830026ba59
Timothy Moore <mtimothy984@gmail.com>
2017-12-29 17:58:32 +0000

    Fix excess WHERE s

--- includes/api/users_helper.php
+++ includes/api/users_helper.php
@@ -173,7 +173,7 @@ class UserParameterParser {
     $result = new SelectQueryCallback('filter_id', array('id' => $id));
 
     $result->where_callback = function($helper) {
-      return 'WHERE users.id = ?';
+      return 'users.id = ?';
     };
 
     $result->bind_where_callback = function($helper) use ($id) {
@@ -199,7 +199,7 @@ class UserParameterParser {
     $result = new SelectQueryCallback('filter_username', array('username' => $username));
 
     $result->where_callback = function($helper) {
-      return 'WHERE users.id IN (SELECT user_id FROM usernames WHERE username LIKE ?)';
+      return 'users.id IN (SELECT user_id FROM usernames WHERE username LIKE ?)';
     };
 
     $result->bind_where_callback = function($helper) use ($username) {
