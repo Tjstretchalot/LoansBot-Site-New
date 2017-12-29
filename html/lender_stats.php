6c03bd2e4d0935877bd7d30a849cc406552044f5
Timothy Moore <mtimothy984@gmail.com>
2017-12-29 21:13:10 +0000

    Add script tag for lender_stats.js in lender_stats.ph

--- html/lender_stats.php
+++ html/lender_stats.php
@@ -44,6 +44,7 @@
     </div>
     <?php include('bootstrap_js.php') ?>
     <script src="js/jquery.basictable.min.js"></script>
+    <script src="js/lender_stats.js"></script>
   </body>
 </html>
 <?php
