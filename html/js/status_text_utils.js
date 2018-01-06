b07fbdb9a7b94a8aa712c80437a2e2a8759923ab
Timothy Moore <mtimothy984@gmail.com>
2018-01-06 18:13:20 +0000

    Fix another selfreference in promise

--- html/js/status_text_utils.js
+++ html/js/status_text_utils.js
@@ -55,12 +55,12 @@ function set_status_text(st_div, new_text, new_alert_type, auto_fold) {
 
  function resolve_with_auto_fold(resolve, reject) {
    var prom2 = new Promise(function(resolve2, reject2) {
-     resolve(prom2);
      if(auto_fold)
        resolve2(setup_auto_fold());
      else
        reject2("auto-folding not requested");
    });
+   resolve(prom2);
  }
 
  var was_hiding = st_div.data("hiding") || false;
