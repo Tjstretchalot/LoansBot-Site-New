ac012b09903b36cf3e26ff59001d52366b748576
Timothy Moore <mtimothy984@gmail.com>
2018-09-04 20:34:13 +0000

    Bug fixes

--- html/red_flags_by_user.php
+++ html/red_flags_by_user.php
@@ -198,19 +198,21 @@
           set_status_text(status_text, SUCCESS_GLYPHICON + ' Found ' + data.reports.length + ' reports', 'success', true, 0);
           for(var i = 0; i < data.reports.length; i++) {
             set_status_text(status_text, LOADING_GLYPHICON + ' Fetching report ' + (i + 1) + '...', 'info', true, 0);
-
-            $.post('/api/red_flag_report.php', { id: data.reports[i].id }, function(data, stat) {
-              set_status_text(status_text, SUCCESS_GLYPHICON + ' Successfully fetched report ' + (i + 1), 'success', true, 0);
-              add_report(data);
-            }).fail(function(xhr) {
-              console.log(xhr.responseJSON);
-              var json_resp = xhr.responseJSON;
-              var err_type = json_resp.errors[0].error_type;
-              var err_mess = json_resp.errors[0].error_message;
-              console.log(err_type + ": " + err_mess);
-              set_status_text(status_text, FAILURE_GLYPHICON + ' Failed fetching report ' + (i + 1) + ': ' + err_mess, 'danger', true, 0);  
-              add_report(xhr.responseJSON);
-            });
+            
+            (function(i) {
+              $.post('/api/red_flag_report.php', { id: data.reports[i].id }, function(data, stat) {
+                set_status_text(status_text, SUCCESS_GLYPHICON + ' Successfully fetched report ' + (i + 1), 'success', true, 0);
+                add_report(data);
+              }).fail(function(xhr) {
+                console.log(xhr.responseJSON);
+                var json_resp = xhr.responseJSON;
+                var err_type = json_resp.errors[0].error_type;
+                var err_mess = json_resp.errors[0].error_message;
+                console.log(err_type + ": " + err_mess);
+                set_status_text(status_text, FAILURE_GLYPHICON + ' Failed fetching report ' + (i + 1) + ': ' + err_mess, 'danger', true, 0);  
+                add_report(xhr.responseJSON);
+              });
+            })(i);
           }
         }).fail(function(xhr) {
           console.log(xhr.responseJSON);
