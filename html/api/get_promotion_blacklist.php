38af387659a63fb2a7b4e6d32f7ad12950c84082
Timothy Moore <mtimothy984@gmail.com>
2019-01-18 16:29:08 -0800

    Undo last commit

--- html/api/get_promotion_blacklist.php
+++ html/api/get_promotion_blacklist.php
@@ -93,13 +93,7 @@ if($_SERVER['REQUEST_METHOD'] === 'GET') {
     $query .= ' AND promo_blacklist_users.id <= ?';
     $args[] = array('i', $max_id);
   }
-
-  if($max_id !== null && $min_id === null) {
-    $query .= ' ORDER BY promo_blacklist_users.id DESC';
-  }else {
-    $query .= ' ORDER BY promo_blacklist_users.id ASC';
-  }
-
+  $query .= ' ORDER BY promo_blacklist_users.id';
   if($limit !== null) {
     $query .= ' LIMIT ?';
     $args[] = array('i', $limit);
